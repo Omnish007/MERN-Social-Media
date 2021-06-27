b374cbe136c6a6359643b83036aadd1f7cd5dbdb
unknown <omnish007@gmail.com>
2021-06-27 09:12:49 +0530

    Modify Code

--- front/src/App.js
+++ front/src/App.js
@@ -1,16 +1,32 @@
+import { useEffect } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
-// import Register  from "./pages/register";
-// import Login  from "./pages/login";
+import Login from "./pages/login";
 import PageRender from "./PageRender";
+import Home from "./pages/home";
+import Notify from "./components/notify/Notify";
+import { useSelector, useDispatch } from "react-redux"
+import { refreshToken } from "./redux/actions/authAction"
+
 
 function App() {
+
+  const { auth} = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(refreshToken())
+  }, [])
+
+
   return (
     <Router>
+      <Notify />
       <input type="checkbox" id="theme" />
       <div className="App">
         <div className="main">
-          <Route path="/:page" component={PageRender} />
-          <Route path="/:page/:id" component={PageRender} />
+          <Route exact path="/" component={auth.token ? Home : Login} />
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:id" component={PageRender} />
 
         </div>
       </div>
---
+++ front/src/redux/actions/authAction.js
@@ -0,0 +1,59 @@
+import { postDataAPI } from "../../utils/fetchData"
+
+export const TYPES = {
+    AUTH : "AUTH"
+}
+
+export const login = (data) => async (dispatch) => {
+    try {
+
+        dispatch({type: "NOTIFY", payload:{loading:true} })
+
+        const res = await postDataAPI("login", data)
+
+        dispatch({
+            type: "AUTH",
+            payload: {
+                token: res.data.access_token,
+                user: res.data.user
+            }
+        })
+
+        localStorage.setItem("firstLogin", true)
+
+        dispatch({
+            type: "NOTIFY",
+            payload: {
+                success: res.data.msg
+            }
+        })
+
+    } catch (error) {
+        dispatch({
+            type: "NOTIFY",
+            payload: {
+                error: error.response.data.msg
+            }
+        })
+    }
+}
+
+export const refreshToken = () => async (dispatch) => {
+    const firstLogin = localStorage.getItem("firstLogin")
+    if(firstLogin){
+        dispatch({type:"NOTIFY", playload:{ loading:true }})
+        try {
+            
+            const res = await postDataAPI("refresh_token")
+            console.log(res)
+
+        } catch (error) {
+            dispatch({
+                type: "NOTIFY",
+                payload: {
+                    error: error.response.data.msg
+                }
+            })
+        }
+    } 
+}
